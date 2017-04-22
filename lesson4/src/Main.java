ed9fb91a2b50b634641481384ff711bd2d274eb2
Vladyslove <od.vladyslav@gmail.com>
2017-04-23 01:48:59 +0300

    Did i do this again?

---
+++ lesson4/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+static int a;
+static char c;
+static double d;
+static float f = 1.0f;
+static long l =1l;
+  public static void main(String[] args) {
+    System.out.println("Wow");
+
+
+    System.out.println(a);
+    System.out.println(c);
+    System.out.println(l);
+    System.out.println(d);
+
+    System.out.println(f);
+
+    double localD= 100.00d;
+    System.out.println(localD);
+
+
+    }   // RELATED TO main
+}     // RELATED TO GENERAL
