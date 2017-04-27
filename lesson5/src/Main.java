22c70c67bb685438224f6cd86973dbb0da02a726
Vladyslove <od.vladyslav@gmail.com>
2017-04-27 11:59:37 +0300

    I changed Main.java file

--- lesson5/src/Main.java
+++ lesson5/src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     System.out.println("Lesson5");
 
     Main main = new Main();
+      // main - link on object in heap
+    // on Stack is created localVariable type (class- is her type) main and
+    // to her assign adrees of new-created object (type- main) in HEAP
 
     Student student = new Student ();
     System.out.println(student.name);
@@ -17,6 +20,10 @@ public class Main {
     Student student3 = new Student ("Vlad");
     System.out.println(student3.name);
     System.out.println(student3.age);
+    student3.bar();
+    student3.bar(1);
+    student3.bar(1, 2);
+    student3.bar(1.4, 2);
 
     // student3.bar();
     // student3.bar();
@@ -31,19 +38,36 @@ public class Main {
     System.out.println(a);
     System.out.println(b);
 
-  }
+    Integer aa = new Integer(102);
+    Integer bb = new Integer(202);
+
+    System.out.println(a);
+    System.out.println(b);
+
+    main.swap2(aa,bb);
+
+    // main.swap(new Integer(14), new Integer(23));
+
+  } // main
 
     void swap (int a, int b) {
     int t = a;
     a = b;
     b = t;
   }
+
+  void swap2 (Integer a, Integer b) {
+  Integer t = a;
+  a = b;
+  b = t;
 }
 
-class TwoIntHolder {
-  int a;
-  int b;
-  }
+} // java
+
+// class TwoIntHolder {
+//   int a;
+//   int b;
+//   }
 
 class Student {
   String name;
@@ -70,6 +94,5 @@ class Student {
   void bar(int i) {}
   void bar(int i, int b) {}
   void bar(double i, int b) {}
-
 
 }
