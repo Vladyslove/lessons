e847a48c5c2493dae4ccde4a1e5b234b106aef3a
Vladyslove <od.vladyslav@gmail.com>
2017-11-27 03:09:00 +0200

    Turning back lesson 31 to its beginning state. Creating lesson 32 on base of lesson 31.

--- lesson31/src/main/java/my/servlet/HelloServlet.java
+++ lesson31/src/main/java/my/servlet/HelloServlet.java
@@ -2,14 +2,13 @@ package my.servlet;
 
 import javax.servlet.http.*;
 import javax.servlet.*;
-import java.io.IOException;
 import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
         response.setContentType("text/html");
 
-        try(PrintWriter out = response.getWriter()) {
+        try (PrintWriter out = response.getWriter()) {
             out.println("<html>");
             out.println("  <head>");
             out.println("   <title>Hello Worm</title>");
@@ -18,8 +17,6 @@ public class HelloServlet extends HttpServlet {
             out.println("   <h2>Hello, Worm!</h2>");
             out.println("  </body>");
             out.println("</html>");
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
